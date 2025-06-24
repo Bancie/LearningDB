cf9eb5a7dbbe48b99256e3788b1502221b15d84c
Bancie <144613141+Bancie@users.noreply.github.com>
2025-06-24 20:01:24 +0700

    Update update_database.sql

--- brain.study/update_database.sql
+++ brain.study/update_database.sql
@@ -290,4 +290,4 @@ MODIFY ENERGY ENUM('exhausted', 'low', 'normal', 'high', 'bursting');
 USE study;
 
 ALTER TABLE ACTIVITY_LOG
-MODIFY SOUND_BACKGROUND ENUM('category_music', 'category_white_noise', 'category_ambient_noise', 'category_silence', 'category_podcast', 'category_construction', 'category_nature', 'source_headphones', 'source_earbuds', 'source_speakers', 'source_public', 'source_private_room');
+MODIFY SOUND_BACKGROUND SET('category_music', 'category_white_noise', 'category_ambient_noise', 'category_silence', 'category_podcast', 'category_construction', 'category_nature', 'source_headphones', 'source_earbuds', 'source_speakers', 'source_public', 'source_private_room');
