83a5d506fb07f7af305010c2f1923eb17fe016dd
Bancie <144613141+Bancie@users.noreply.github.com>
2026-02-14 17:45:03 +0700

    Learning updated

--- learning/main.py
+++ learning/main.py
@@ -1,30 +1,23 @@
-# from fastapi import FastAPI
-# from pydantic import BaseModel
+from enum import Enum
 
-# app = FastAPI()
-
-
-# class Item(BaseModel):
-#     name: str
-#     price: float
-#     is_offer: bool | None = None
+from fastapi import FastAPI
 
 
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: str | None = None):
-#     return {"item_id": item_id, "q": q}
+app = FastAPI()
 
 
-# @app.put("/items/{item_id}")
-# def update_item(item_id: int, item: Item):
-#     return {"item_name": item.name, "item_id": item_id}
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
-import os
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
 
-name = os.getenv("MY_NAME")
-print(f"Hello {name} from Python")
+    return {"model_name": model_name, "message": "Have some residuals"}
